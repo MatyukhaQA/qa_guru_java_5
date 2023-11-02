fc7b77e7b4855639c10ae0fea7aa5f71fbf61a3a
masha <m@matyuha.ru>
2023-11-02 23:09:13 +0300

    updated test

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -1,3 +1,5 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -6,11 +8,15 @@ import static com.codeborne.selenide.Selenide.*;
 
 
 public class DragAndDropTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://the-internet.herokuapp.com/";
+        Configuration.browserSize = "1500x1000";
+    }
 
     @Test
     void dragAndDropWithMoveToElementTest() {
-        open("https://the-internet.herokuapp.com/drag_and_drop");
-//        actions().dragAndDrop()
+        open("drag_and_drop");
         actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(150, 0).release().perform();
         $("#column-a").shouldHave(text("B"));
         $("#column-b").shouldHave(text("A"));
@@ -18,8 +24,7 @@ public class DragAndDropTest {
 
     @Test
     void dragAndDropTest() {
-        open("https://the-internet.herokuapp.com/drag_and_drop");
-//        actions().dragAndDrop()
+        open("drag_and_drop");
         $("#column-a").dragAndDrop(to($("#column-b")));
         $("#column-a").shouldHave(text("B"));
         $("#column-b").shouldHave(text("A"));
